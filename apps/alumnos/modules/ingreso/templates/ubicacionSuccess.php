6c698cf022a41043da2145343d9943836021668e
Diego <diego@Mate>
2016-09-19 09:30:31 -0300

    cambios

--- apps/alumnos/modules/ingreso/templates/ubicacionSuccess.php
+++ apps/alumnos/modules/ingreso/templates/ubicacionSuccess.php
@@ -1,6 +1,12 @@
 <div class="post">
-					<h2>SALUDENT</h2>
-					<h2>Historia</h2>
-					<p><strong>Fundado el 03/12/1979</strong> en la ciudad de Concepción del Uruguay, Provincia de Entre Ríos.<strong> Todos los presentes involucrados atestiguan el acontencimiento..</strong> Bienvenidos al sitio web online. </p>
-					<p><strong>Circulo Odontogócio Concepcion del Uruguay.</strong> En la ciudad de Concepción del Uruguay, Provincia de Entre Ríos, a los 3 días del mes de Diciembre de 1999 y siendo las 19 y 1/2 horas, los Doctores en Odontología y Dentistas al margen anotados reunidos en la casa del Dr. Víctor F. Perez y con la conformidad de los Dres. Perez Sosa, exteriorizando un anhelo colectivo dentro de la profesión de constituir una asociación de sus colegas con los objetivos más elevados en bien de los intereses morales, intelectuales y materiales de la profesión común, resolvieron constituir dicha asociación. A moción de la Dra. Lopez designó Presidente Provisional al Dr. Perez y Secretario al Dr. Miguel Gonzalez. Acto continuo el Dr. Perez presenta la moción, que fue aceptada, de que este centro llevara el nombre de 'Círculo Odontológico Concepción del Uruguay'...“</p>
+			<h2 class="title">Datos de contacto</h2>
+			<p><b>Dirección: </b>Rocamora 471, E.R., Argentina</p>
+			<p><abbr title="Phone">Tel</abbr>: +54 3442 42-2957</p>
+			<p><abbr title="Email">Email</abbr>: <a href="mailto:gokazanski@gmail.com">gokazanski@gmail.com</a>
+			</p>
+			<p><abbr title="Hours">Horarios</abbr>: Lunes - Viernes: 10 a 19 hs</p>
+			<br>
+</div>
+<div class="post">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3365.631983048911!2d-58.23081664963288!3d-32.482533855780744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95afdb0044b7bffb%3A0x1ac46e06ea30db49!2sCirculo+Odontologico!5e0!3m2!1ses-419!2sar!4v1474287530550" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
