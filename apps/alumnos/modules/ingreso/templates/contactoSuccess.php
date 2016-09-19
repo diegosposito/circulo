702def859acbd90c26e7642bdac09fe8249befeb
Diego <diego@Mate>
2016-09-19 09:28:06 -0300

    contacot

--- apps/alumnos/modules/ingreso/templates/contactoSuccess.php
+++ apps/alumnos/modules/ingreso/templates/contactoSuccess.php
@@ -1,13 +1,13 @@
 <div class="post">
 			<h2 class="title">Datos de contacto</h2>
-			<p><b>Dirección: </b>Av. Directorio 455 C.A.B.A., Argentina ( C1424CIE )</p>
-			<p><abbr title="Phone">Tel</abbr>: +54 11 4923 2085</p>
-			<p><abbr title="Email">Email</abbr>: <a href="mailto:info@laboratoriolaca.com">info@laboratoriolaca.com</a>
+			<p><b>Dirección: </b>Rocamora 471, E.R., Argentina</p>
+			<p><abbr title="Phone">Tel</abbr>: +54 3442 42-2957</p>
+			<p><abbr title="Email">Email</abbr>: <a href="mailto:gokazanski@gmail.com">gokazanski@gmail.com</a>
 			</p>
 			<p><abbr title="Hours">Horarios</abbr>: Lunes - Viernes: 10 a 19 hs</p>
 			<br>
 </div>
 <div class="post">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3283.060439960341!2d-58.433151099999996!3d-34.6279129!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca4c17e8af5b%3A0x11092cc840beb498!2sAv+Directorio+455%2C+Buenos+Aires%2C+Ciudad+Aut%C3%B3noma+de+Buenos+Aires!5e0!3m2!1ses-419!2sar!4v1420397418455" width="100%" height="450" frameborder="0" style="border:0"></iframe>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3365.631983048911!2d-58.23081664963288!3d-32.482533855780744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95afdb0044b7bffb%3A0x1ac46e06ea30db49!2sCirculo+Odontologico!5e0!3m2!1ses-419!2sar!4v1474287530550" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
 
