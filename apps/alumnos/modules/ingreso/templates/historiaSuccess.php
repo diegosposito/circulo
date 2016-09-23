33e792a4866d6e67bdd8ffd8640d74275861930f
diegosposito <spositod@gmail.com>
2016-09-23 18:43:35 -0300

    historia

--- apps/alumnos/modules/ingreso/templates/historiaSuccess.php
+++ apps/alumnos/modules/ingreso/templates/historiaSuccess.php
@@ -1,6 +1,26 @@
 <div class="post">
 					<h2>CIRCULO ODONTOLOGICO Concepción del Uruguay</h2>
 					<h2>Historia</h2>
-					<p><strong>Fundado el 03/12/1979</strong> en la ciudad de Concepción del Uruguay, Provincia de Entre Ríos.<strong> Todos los presentes involucrados atestiguan el acontencimiento..</strong> Bienvenidos al sitio web online. </p>
-					<p><strong>Circulo Odontogócio Concepcion del Uruguay.</strong> En la ciudad de Concepción del Uruguay, Provincia de Entre Ríos, a los 3 días del mes de Diciembre de 1999 y siendo las 19 y 1/2 horas, los Doctores en Odontología y Dentistas al margen anotados reunidos en la casa del Dr. Víctor F. Perez y con la conformidad de los Dres. Perez Sosa, exteriorizando un anhelo colectivo dentro de la profesión de constituir una asociación de sus colegas con los objetivos más elevados en bien de los intereses morales, intelectuales y materiales de la profesión común, resolvieron constituir dicha asociación. A moción de la Dra. Lopez designó Presidente Provisional al Dr. Perez y Secretario al Dr. Miguel Gonzalez. Acto continuo el Dr. Perez presenta la moción, que fue aceptada, de que este centro llevara el nombre de 'Círculo Odontológico Concepción del Uruguay'...“</p>
+					<p>Con la búsqueda de desarrollar la acción gremial, científica, cultural y social, teniendo por objetivo la dignificación de los <strong>Profesionales Odontólogos del Departamento Uruguay</strong>, en el año 1957 se constituye el <strong>Círculo Odontológico.</strong></p>
+					<p><strong>Acta Nro 1</strong></p>
+
+                    <p>En la Ciudad de <strong>Concepción del Uruguay</strong>, a los 12 días del mes de marzo de 1957, queda reorganizado el <strong>Círculo Odontológico de Concepción del Uruguay (C.O.C.U.).</strong> Redactado y aprobado su estatuto y Comisión Directiva, por voto de la mayoría de los Socios que firman el Acta, quedando formada de la siguiente manera:
+					<br><br><strong>Presidente:</strong> Dr. Juan José Rosso
+					<br><strong>Secretario:</strong> Dr. René Punzi
+					<br><strong>Tesorero:</strong> Dr. Héctor Palma Parodi
+					<br><strong>1º Vocal:</strong> Dr. Máximo Alberto Schmidt
+					<br><strong>2º Vocal:</strong> Dr. Julio Artusi
+					<br>
+					<p><strong>Firman el acta los siguientes Socios:</strong></p>
+                    <p>Juan José Rosso – René Punzi – Héctor Palma Parodi – Máximo Alberto Schmidt – Julio Artusi – Dora Santini – Carlos María Bernasconi – Francisco Nano – Rodolfo Rodríguez Decurgez – Arnoldo Erramuspe – Raúl Erramuspe Horacio – Jacobo Kuperman – Dionisio Nano. </p>
+                    <br>
+                    
+                    <table width="100%" border="5" bordercolor="#7DBF0D" align="center">
+				    <tr>
+				        <td><img src='<?php echo $sf_request->getRelativeUrlRoot();?>/images/titulo.png' height="380px" width="300px"></img></td>
+				        <td><img src='<?php echo $sf_request->getRelativeUrlRoot();?>/images/placa.png' height="380px" width="300px"></img></td>
+				        
+				    </tr>
+				    </table>		
+
 </div>
