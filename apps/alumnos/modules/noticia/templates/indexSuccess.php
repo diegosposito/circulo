fe935d16b0d72bb71e6b53ba83926fabe7d3a5cc
diegosposito <spositod@gmail.com>
2016-10-06 19:07:10 -0300

    cabmios

--- apps/alumnos/modules/noticia/templates/indexSuccess.php
+++ apps/alumnos/modules/noticia/templates/indexSuccess.php
@@ -5,16 +5,11 @@
     <tr>
       <th>Id</th>
       <th>Titulo</th>
-      <th>Descripcion</th>
       <th>Copete</th>
       <th>Idtiponoticia</th>
       <th>Visible</th>
       <th>Idorden</th>
-      <th>Imagefile</th>
-      <th>Created at</th>
-      <th>Updated at</th>
-      <th>Created by</th>
-      <th>Updated by</th>
+      <th>Acciones</th>
     </tr>
   </thead>
   <tbody>
@@ -22,19 +17,14 @@
     <tr>
       <td><a href="<?php echo url_for('noticia/show?id='.$noticia->getId()) ?>"><?php echo $noticia->getId() ?></a></td>
       <td><?php echo $noticia->getTitulo() ?></td>
-      <td><?php echo $noticia->getDescripcion() ?></td>
       <td><?php echo $noticia->getCopete() ?></td>
-      <td><?php echo $noticia->getIdtiponoticia() ?></td>
-      <td><?php echo $noticia->getVisible() ?></td>
+      <td><?php echo $noticia->getIdtiponoticia() =='1' ? 'General' : 'De Profesionales' ?></td>
+      <td><?php echo $noticia->getVisible() ? 'Visible' : 'No Visible' ?></td>
       <td><?php echo $noticia->getIdorden() ?></td>
-      <td><?php echo $noticia->getImagefile() ?></td>
-      <td><?php echo $noticia->getCreatedAt() ?></td>
-      <td><?php echo $noticia->getUpdatedAt() ?></td>
-      <td><?php echo $noticia->getCreatedBy() ?></td>
-      <td><?php echo $noticia->getUpdatedBy() ?></td>
+      <td><a href="<?php echo url_for('noticia/edit?id='.$noticia->getId()) ?>"><?php echo 'Editar' ?></a></td>
     </tr>
     <?php endforeach; ?>
   </tbody>
 </table>
 
-  <a href="<?php echo url_for('noticia/new') ?>">New</a>
+  <a href="<?php echo url_for('noticia/new') ?>">Nueva noticia</a>
