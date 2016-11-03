7f297eb9ea60c9ddd5c5223f6aa056f31711aaff
Diego <diego@Mate>
2016-11-03 08:47:09 -0300

    cambio

--- apps/alumnos/modules/informes/templates/saludentSuccess.php
+++ apps/alumnos/modules/informes/templates/saludentSuccess.php
@@ -2,22 +2,22 @@
     p { margin-left:5em; /* Or another measurement unit, like px */ }
   </style>
   <br>
-  <h1 style="color:#7dbf0d;font-size:24px">Institucionales </h1>
-  <h2 style="color:#7dbf0d;font-size:20px">Saludent </h2>
-
+  <h1 style="align:center;color:#7dbf0d;font-size:24px">Institucionales -> Saludent </h1>
+  
 <div align="center"><img src='<?php echo $sf_request->getRelativeUrlRoot();?>/images/saludent.jpg' width='200px' height='150px' />
 </div>
-<p>Es el nombre del Sistema de prestaciones de Salud odontológico solidario, creado por el <strong>Círculo Odontológico de Concepción del Uruguay.</strong>
-<br>Tiene como característica fundamental poner a disposición del afiliado la libre elección del profesional odontólogo en el ámbito del Departamento Uruguay.
+
+<p style="margin-left: 3em;">Es el nombre del Sistema de prestaciones de Salud odontológico solidario, creado por el <strong>Círculo Odontológico de Concepción del Uruguay.</strong>
+<br>Tiene como característica fundamental poner a disposición del afiliado la libre elección del profesional odontólogo en el ámbito del Departamento Uruguay.</p>
+<?php foreach ($contenidoss as $contenido){ ?>
+<p style="margin-left: 3em;"><?php echo html_entity_decode($contenido->getDescripcion()); ?></p>
+<?php } ?>
 <br>
-<?php foreach ($contenidoss as $contenido){ 
-echo html_entity_decode($contenido->getDescripcion()); 
-}
-?>
-<br>
+<div align="center">
 <p><strong>NOMENCLADOR Y COSEGUROS</strong></p>
 
  <?php $idtipopractica = 0; ?>
+
 
 <?php foreach ($practicass as $practicas){ 
          if($idtipopractica<>0 && $practicas->getIdtipopractica()<>$idtipopractica){ ?>
@@ -30,7 +30,7 @@ echo html_entity_decode($contenido->getDescripcion());
             $idtipopractica = $practicas->getIdtipopractica();
             $i=0; ?>
 
-              <table align="center" width="550px" cellspacing="0" class="stats">
+              <table style="align:center;padding-left:10px;" width="550px" cellspacing="0" class="stats">
               <thead>
                <tr>
                <td colspan="3" width="100%" align="left" class="hed"><?php echo $practicas->getTipoPracticas()->getNombre(); ?></td>
@@ -50,9 +50,10 @@ echo html_entity_decode($contenido->getDescripcion());
 
        
   </tbody>
-  </table>  
+  </table> 
+</div>   
 <br>
- <p>Urgencias Odontológicas: 15 629 345 </p>
+ <p style="margin-left: 3em;">Urgencias Odontológicas: 15 629 345 </p>
  <br>
- <p><strong>Promoción: Pago de cuotas, Tratamientos de Prótesis, Implantes, Ortodoncia, Ortopedia
+ <p style="margin-left: 3em;"><strong>Promoción: Pago de cuotas, Tratamientos de Prótesis, Implantes, Ortodoncia, Ortopedia
  con Tarjeta de Crédito hasta 12 cuotas </strong></p>
