61f751276df6649a71627cbfce6a022262f12360
Diego <diego@Mate>
2016-12-07 11:46:13 -0300

    cambios

--- lib/form/doctrine/PacientesForm.class.php
+++ lib/form/doctrine/PacientesForm.class.php
@@ -14,5 +14,50 @@ class PacientesForm extends BasePacientesForm
   {
 
   	     unset( $this['created_at'], $this['updated_at'], $this['created_by'], $this['updated_by'] );
+
+  	     // Se define los labels
+	    $this->widgetSchema->setLabel('nombre', '<p align="left">Nombre:</p>');
+ 	    $this->widgetSchema->setLabel('apellido', '<p align="left">Apellido:</p>');
+        $this->widgetSchema->setLabel('nroafiliado', '<p align="left">Nro. Afiliado:</p>');
+ 	    $this->widgetSchema->setLabel('nrodoc', '<p align="left">Nro. Documento:</p>');
+        $this->widgetSchema->setLabel('idsexo', '<p align="left">Sexo:</p>');
+        $this->widgetSchema->setLabel('estadocivil', '<p align="left">E. Civil:</p>');
+        $this->widgetSchema->setLabel('fechanac', '<p align="left">Fec. Nacimiento:</p>');
+        $this->widgetSchema->setLabel('fechaingreso', '<p align="left">Fec. Ingreso:</p>');
+        $this->widgetSchema->setLabel('direccion', '<p align="left">Dirección:</p>');
+
+        $this->widgetSchema->setLabel('idciudadnac', '<p align="left">Ciudad Nacimiento:</p>');
+ 	    $this->widgetSchema->setLabel('email', '<p align="left">Email:</p>');
+        $this->widgetSchema->setLabel('celular', '<p align="left">Celular:</p>');
+ 	    $this->widgetSchema->setLabel('telefono', '<p align="left">Teléfono:</p>');
+        $this->widgetSchema->setLabel('titular', '<p align="left">Titular:</p>');
+        $this->widgetSchema->setLabel('parentesco', '<p align="left">Parentesco:</p>');
+        $this->widgetSchema->setLabel('ocupacion', '<p align="left">Ocupación:</p>');
+        $this->widgetSchema->setLabel('siglas', '<p align="left">Siglas:</p>');
+        $this->widgetSchema->setLabel('plan', '<p align="left">Plan:</p>');
+        $this->widgetSchema->setLabel('trabajo', '<p align="left">Trabajo:</p>');
+        $this->widgetSchema->setLabel('jerarquia', '<p align="left">Jerarquía:</p>');
+        $this->widgetSchema->setLabel('credencial', '<p align="left">Credencial:</p>');
+        $this->widgetSchema->setLabel('anotaciones', '<p align="left">Anotaciones:</p>');
+        $this->widgetSchema->setLabel('activo', '<p align="left">Activo:</p>');
+        $this->widgetSchema->setLabel('historial', '<p align="left">Historial:</p>');
+
+        $this->widgetSchema['imagefile'] = new sfWidgetFormInputFile(array(
+                                               'label' => 'Archivo',
+                                             ));
+         $this->widgetSchema->setLabel('imagefile', '<p align="left">Foto:</p>');
+         
+        $range  = range(date('Y')-80, date('Y')+1);
+		$years = array_combine($range,$range);
+
+		$this->widgetSchema['fechanac'] =
+		     new sfWidgetFormDate(array('years' => $years));
+		$this->widgetSchema['fechaingreso'] =
+		     new sfWidgetFormDate(array('years' => $years));     
+
+		$this->widgetSchema->setLabel('fechanac', '<p align="left">Fec. Nacimiento:</p>');
+        $this->widgetSchema->setLabel('fechaingreso', '<p align="left">Fec. Ingreso:</p>');
+            
+      
   }
 }
