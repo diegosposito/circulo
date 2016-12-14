5089babfa70e8f16d967ff81a78c9a6801bfb97a
diego <spositod@gmail.com>
2016-12-14 14:57:20 -0300

    cambios

--- apps/alumnos/modules/pacientes/templates/_form.php
+++ apps/alumnos/modules/pacientes/templates/_form.php
@@ -19,114 +19,73 @@
     <tbody>
           <?php echo $form->renderGlobalErrors() ?>
         <tr>
-          <td width="11%">
-            <?php echo $form['nroafiliado']->renderLabel() ?>
+        <th align="center" colspan=4><?php echo ''.'D A T O S &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;P E R S O N A L E S ' ?></th>
+        </tr>  
+        <tr>
+          <td width="10%">
+            <?php echo "<b>".$form['nroafiliado']->renderLabel()."</b>" ?>
           </td>
           <td>
             <?php echo $form['nroafiliado']->render() ?>
           </td>
+           <td rowspan="5" width="10%">
+          </td>
+           <td rowspan="5" width="10%">
+            <img style="align:center; width: 180px; height: 180px;" src='<?php echo $sf_request->getRelativeUrlRoot();?>/files/pacientes/3/angelina.jpg' align='center' size='20' />
+          </td>
+        </tr>  
+        <tr>
           <td width="10%">
-            <?php echo $form['apellido']->renderLabel() ?>
+            <?php echo "<b>".$form['nombre']->renderLabel()."</b>" ?>
+          </td>
+          <td>
+            <?php echo $form['nombre']->render() ?>
+          </td>
+        </tr>  
+        <tr>
+          <td width="10%">
+            <?php echo "<b>".$form['apellido']->renderLabel()."</b>" ?>
           </td>
           <td>
             <?php echo $form['apellido']->render() ?>
           </td>
-
+        </tr> 
+        <tr>
+          <td width="10%">
+            <?php echo "<b>".$form['nrodoc']->renderLabel()."</b>" ?>
+          </td>
+          <td>
+            <?php echo $form['nrodoc']->render() ?>
+          </td>
         </tr>  
-
-      <tr>
-        <td colspan="4"><br></td>
-      </tr>        
-      <tr>
-        <td colspan="2">
-          <?php echo $form['nombre']->renderError() ?>
-        </td>
-        <td colspan="2">
-          <?php echo $form['apellido']->renderError() ?>
-        </td>
-      </tr>
-      <tr>
-        <td><?php echo "<b>".$form['nombre']->renderLabel()."</b>" ?></td>
-        <td><?php echo $form['nombre'] ?></td>
-        <td align="left"><?php echo "<b>".$form['apellido']->renderLabel()."</b>" ?></td>
-        <td align="left"><?php echo $form['apellido'] ?></td>
-      </tr>  
-      <tr>
-        <td colspan="4"><?php echo $form['nrodoc']->renderError() ?></td>
-      </tr> 
-      <tr>
-        <td align="left"><?php echo "<b>".$form['nrodoc']->renderLabel()."</b>" ?></td>
-        <td align="left"><?php echo $form['nrodoc'] ?></td>
-        <td colspan=2> </td>
-      </tr> 
-      <tr>
-        <td colspan="2">
-          <?php echo $form['idsexo']->renderError() ?>
-        </td>
-        <td colspan="2">
-          <?php echo $form['estadocivil']->renderError() ?>
-        </td>
-      </tr>
+        <tr>
+          <td width="10%">
+            <?php echo "<b>".$form['fechanac']->renderLabel()."</b>" ?>
+          </td>
+          <td>
+            <?php echo $form['fechanac']->render() ?>
+          </td>
+        </tr>   
       <tr>
         <td><?php echo "<b>".$form['idsexo']->renderLabel()."</b>" ?></td>
         <td><?php echo $form['idsexo'] ?></td>
         <td align="left"><?php echo "<b>".$form['estadocivil']->renderLabel()."</b>" ?></td>
         <td align="left"><?php echo $form['estadocivil'] ?></td>
       </tr>  
-       <tr>
-        <td colspan="2">
-          <?php echo $form['fechanac']->renderError() ?>
-        </td>
-        <td colspan="2">
-          <?php echo $form['fechaingreso']->renderError() ?>
-        </td>
-      </tr>
       <tr>
-        <td><?php echo "<b>".$form['fechanac']->renderLabel()."</b>" ?></td>
-        <td><?php echo $form['fechanac'] ?></td>
-        <td align="left"><?php echo "<b>".$form['fechaingreso']->renderLabel()."</b>" ?></td>
-        <td align="left"><?php echo $form['fechaingreso'] ?></td>
-      </tr>  
-      <tr>
-        <th align="center" colspan=3><?php echo '<br>'.'D O M I C I L I O ' ?></th>
-      </tr>
-      <tr>
-        <td colspan="4">
-          <?php echo $form['direccion']->renderError() ?>
-        </td>
-      </tr>
-       <tr>
         <td colspan="1"><?php echo "<b>".$form['direccion']->renderLabel()."</b>" ?></td>
         <td colspan="3"><?php echo $form['direccion'] ?></td>
       </tr>  
-      <tr>
-        <td colspan="2">
-          <?php echo $form['idciudadnac']->renderError() ?>
-        </td>
-      </tr>
        <tr>
         <td colspan="1"><?php echo "<b>".$form['idciudadnac']->renderLabel()."</b>" ?></td>
         <td colspan="3"><?php echo $form['idciudadnac'] ?></td>
       </tr> 
        <tr>
-        <td colspan="2">
-          <?php echo $form['idciudadnac']->renderError() ?>
-        </td>
-      </tr>
-       <tr>
         <td colspan="1"><?php echo "<b>".$form['idciudadnac']->renderLabel()."</b>" ?></td>
         <td colspan="3"><?php echo $form['idciudadnac'] ?></td>
       </tr>  
       <tr>
-        <th align="center" colspan=4><?php echo '<br>'.'D A T O S   C O N T A C T O ' ?></th>
-      </tr>
-       <tr>
-        <td colspan="2">
-          <?php echo $form['email']->renderError() ?>
-        </td>
-        <td colspan="2">
-          <?php echo $form['celular']->renderError() ?>
-        </td>
+        <th align="center" colspan=4><?php echo ''.'D A T O S &nbsp;&nbsp;&nbsp;C O N T A C T O ' ?></th>
       </tr>
        <tr>
         <td><?php echo "<b>".$form['email']->renderLabel()."</b>" ?></td>
@@ -135,26 +94,15 @@
         <td align="left"><?php echo $form['celular'] ?></td>
       </tr> 
        <tr>
-        <td colspan="4">
-          <?php echo $form['telefono']->renderError() ?>
-        </td>
-      </tr>
-       <tr>
         <td><?php echo "<b>".$form['telefono']->renderLabel()."</b>" ?></td>
         <td><?php echo $form['telefono'] ?></td>
         <td align="left"><?php echo '' ?></td>
         <td align="left"><?php echo '' ?></td>
       </tr> 
       <tr>
-        <th align="center" colspan=4><?php echo '<br>'.'O T R A    I N F O R M A C I O N ' ?></th>
       </tr>
-       <tr>
-        <td colspan="2">
-          <?php echo $form['titular']->renderError() ?>
-        </td>
-        <td colspan="2">
-          <?php echo $form['parentesco']->renderError() ?>
-        </td>
+      <tr>
+        <th align="center" colspan=4><?php echo ''.'O T R A &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I N F O R M A C I O N ' ?></th>
       </tr>
        <tr>
         <td><?php echo "<b>".$form['titular']->renderLabel()."</b>" ?></td>
@@ -163,27 +111,11 @@
         <td align="left"><?php echo $form['parentesco'] ?></td>
       </tr> 
        <tr>
-        <td colspan="2">
-          <?php echo $form['ocupacion']->renderError() ?>
-        </td>
-        <td colspan="2">
-          <?php echo $form['siglas']->renderError() ?>
-        </td>
-      </tr>
-       <tr>
         <td><?php echo "<b>".$form['ocupacion']->renderLabel()."</b>" ?></td>
         <td><?php echo $form['ocupacion'] ?></td>
         <td align="left"><?php echo "<b>".$form['siglas']->renderLabel()."</b>" ?></td>
         <td align="left"><?php echo $form['siglas'] ?></td>
       </tr> 
-       <tr>
-        <td colspan="2">
-          <?php echo $form['plan']->renderError() ?>
-        </td>
-        <td colspan="2">
-          <?php echo $form['trabajo']->renderError() ?>
-        </td>
-      </tr>
        <tr>
         <td><?php echo "<b>".$form['plan']->renderLabel()."</b>" ?></td>
         <td><?php echo $form['plan'] ?></td>
@@ -191,54 +123,25 @@
         <td align="left"><?php echo $form['trabajo'] ?></td>
       </tr> 
        <tr>
-        <td colspan="2">
-          <?php echo $form['jerarquia']->renderError() ?>
-        </td>
-        <td colspan="2">
-          <?php echo $form['credencial']->renderError() ?>
-        </td>
-      </tr>
-       <tr>
         <td><?php echo "<b>".$form['jerarquia']->renderLabel()."</b>" ?></td>
         <td><?php echo $form['jerarquia'] ?></td>
         <td align="left"><?php echo "<b>".$form['credencial']->renderLabel()."</b>" ?></td>
         <td align="left"><?php echo $form['credencial'] ?></td>
       </tr> 
        <tr>
-        <td colspan="2">
-          <?php echo $form['anotaciones']->renderError() ?>
-        </td>
-        <td colspan="2">
-          <?php echo $form['activo']->renderError() ?>
-        </td>
-      </tr>
-       <tr>
         <td><?php echo "<b>".$form['anotaciones']->renderLabel()."</b>" ?></td>
         <td><?php echo $form['anotaciones'] ?></td>
         <td align="left"><?php echo "<b>".$form['activo']->renderLabel()."</b>" ?></td>
         <td align="left"><?php echo $form['activo'] ?></td>
-      </tr>
-      <tr>
-        <td colspan="2">
-          <?php echo $form['historial']->renderError() ?>
-        </td>
       </tr>
        <tr>
         <td colspan="1"><?php echo "<b>".$form['historial']->renderLabel()."</b>" ?></td>
         <td colspan="3"><?php echo $form['historial'] ?></td>
       </tr>  
        <tr>
-        <td colspan="2">
-          <?php echo $form['imagefile']->renderError() ?>
-        </td>
-      </tr>
-       <tr>
         <td align="left"><?php echo "<b>".$form['imagefile']->renderLabel()."</b>" ?></td>
         <td  colspan="3" align="left"><?php echo $form['imagefile'] ?></td>
       </tr>  
-     
-      
-      
                 
     </tbody>
   </table>
--- apps/alumnos/modules/pacientes/templates/editSuccess.php
+++ apps/alumnos/modules/pacientes/templates/editSuccess.php
@@ -1,10 +1,79 @@
- <style type="text/css">
+<style type="text/css">
+    body {font-family: "Lato", sans-serif;}
+
+	ul.tab {
+	    list-style-type: none;
+	    margin: 0;
+	    padding: 0;
+	    overflow: hidden;
+	    border: 1px solid #ccc;
+	    background-color: #f1f1f1;
+	}
+
+	/* Float the list items side by side */
+	ul.tab li {float: left;}
+
+	/* Style the links inside the list items */
+	ul.tab li a {
+	    display: inline-block;
+	    color: black;
+	    text-align: center;
+	    padding: 14px 16px;
+	    text-decoration: none;
+	    transition: 0.3s;
+	    font-size: 17px;
+	}
+
+	/* Change background color of links on hover */
+	ul.tab li a:hover {
+	    background-color: #ddd;
+	}
+
+	/* Create an active/current tablink class */
+	ul.tab li a:focus, .active {
+	    background-color: #ccc;
+	}
+
+	/* Style the tab content */
+	.tabcontent {
+	    display: none;
+	    padding: 6px 12px;
+	    border: 1px solid #ccc;
+	    border-top: none;
+	}
     p { margin-left:5em; /* Or another measurement unit, like px */ }
   </style>
 
+  <script>
+
+	function openCity(evt, cityName) {
+	    var i, tabcontent, tablinks;
+	    tabcontent = document.getElementsByClassName("tabcontent");
+	    for (i = 0; i < tabcontent.length; i++) {
+	        tabcontent[i].style.display = "none";
+	    }
+	    tablinks = document.getElementsByClassName("tablinks");
+	    for (i = 0; i < tablinks.length; i++) {
+	        tablinks[i].className = tablinks[i].className.replace(" active", "");
+	    }
+	    document.getElementById(cityName).style.display = "block";
+	    evt.currentTarget.className += " active";
+	}
+
+</script>
+
 <br>
-<h1 align="center" style="color:black;">Editar pacientes</h1>
+<h1 align="center" style="color:black;">Editar Pacientes</h1>
 <br>
 
+<ul class="tab">
+  <li><a href="javascript:void(0)" class="tablinks" onclick="openCity(event, 'Datos')">Datos Personales</a></li>
+  <li><a href="javascript:void(0)" class="tablinks" onclick="openCity(event, 'Contacto')">Contacto</a></li>
+  <li><a href="javascript:void(0)" class="tablinks" onclick="openCity(event, 'Documentacion')">Documentacion</a></li>
+  <li><a href="javascript:void(0)" class="tablinks" onclick="openCity(event, 'Guardar')">Guardar</a></li>
+</ul>
+
+<div id="Datos" class="tabcontent">
 <?php include_partial('form', array('form' => $form)) ?>
+ </div>
 
