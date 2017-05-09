54adce3c283de8c9fa11797f2858e71b16d21b5a
Sposito, Diego <spositod@gmail.com>
2017-05-09 09:49:06 -0300

    cabmios

--- lib/form/doctrine/TratamientosForm.class.php
+++ lib/form/doctrine/TratamientosForm.class.php
@@ -31,21 +31,29 @@ class TratamientosForm extends BaseTratamientosForm
     $this->widgetSchema->setLabel('descripcion', '<p align="left">Descripción:</p>');
  	$this->widgetSchema->setLabel('normas', '<p align="left">Normas de Trabajo:</p>');
  	$this->widgetSchema->setLabel('activo', '<p align="left">Activo?:</p>');
-    
+
     $oss = Doctrine_Core::getTable('ObrasSociales')->obtenerTodas();
     foreach($oss as $os){
-        $arregloOS[$os->getIdObrasocial()] = $os->getAbreviada(); 
-    }  
-       
+        $arregloOS[$os->getIdObrasocial()] = $os->getAbreviada();
+    }
+
     $this->widgetSchema['idobrasocial'] = new sfWidgetFormSelect(array('choices' => $arregloOS));
     $this->widgetSchema->setLabel('idobrasocial', '<p align="left">Obra Social:</p>');
 
-   
+    $oss = Doctrine_Core::getTable('GrupoTratamiento')->obtenerTodos();
+    foreach($oss as $os){
+        $arregloGT[$os->getId()] = $os->getNombre()."(".$os->getAbreviacion().")";
+    }
+
+    $this->widgetSchema['idgrupotratamiento'] = new sfWidgetFormSelect(array('choices' => $arregloGT));
+    $this->widgetSchema->setLabel('idgrupotratamiento', '<p align="left">Grupo Tratamiento:</p>');
+
+
     // arreglos estaticos
-    $arregloAutorizacion = array('1' => 'No Autoriza', '2' => 'Autorización Previa','3' => 'Con Presentación de Orden', '4' => 'Con Bono de COCU'); 
-    $arregloVisible = array('1' => 'Si', '2' => 'No'); 
-    $arregloOdontologia = array('1' => 'General', '2' => 'Prótesis','3' => 'Implante', '4' => 'Ortodoncia'); 
-      
+    $arregloAutorizacion = array('1' => 'No Autoriza', '2' => 'Autorización Previa','3' => 'Con Presentación de Orden', '4' => 'Con Bono de COCU');
+    $arregloVisible = array('1' => 'Si', '2' => 'No');
+    $arregloOdontologia = array('1' => 'General', '2' => 'Prótesis','3' => 'Implante', '4' => 'Ortodoncia');
+
     $this->widgetSchema['idautorizacion'] = new sfWidgetFormSelect(array('choices' => $arregloAutorizacion));
     $this->widgetSchema->setLabel('idautorizacion', '<p align="left">Autorización:</p>');
 
@@ -54,7 +62,7 @@ class TratamientosForm extends BaseTratamientosForm
 
     $this->widgetSchema['idontologia'] = new sfWidgetFormSelect(array('choices' => $arregloOdontologia));
     $this->widgetSchema->setLabel('idontologia', '<p align="left">Odontología:</p>');
-    
+
     $this->widgetSchema['nombre'] = new sfWidgetFormInputText(array(), array("style"=>'width: 250px;'));
     $this->widgetSchema->setLabel('nombre', '<p align="left">Tratamiento:</p>');
 
@@ -77,8 +85,8 @@ class TratamientosForm extends BaseTratamientosForm
       'activo'             => new sfValidatorBoolean(array('required' => false)),
     ));
 
-    $this->validatorSchema->setOption('allow_extra_fields',true); 
+    $this->validatorSchema->setOption('allow_extra_fields',true);
 
-  
+
   }
 }
