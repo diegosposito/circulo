861cc0535b5ac264576f9d0bc99274c3b8f529d6
diegosposito <spositod@gmail.com>
2017-05-26 21:42:54 -0300

    cambios

--- apps/alumnos/modules/atenciones/templates/_formnew.php
+++ apps/alumnos/modules/atenciones/templates/_formnew.php
@@ -1,7 +1,73 @@
+
+<script>
+$(document).ready(function(){
+
+    $('#atenciones_importe').attr('disabled','disabled');
+    $('#atenciones_coseguro').attr('disabled','disabled');
+
+    $('#botonGuardarAtencion').click(function() {
+      var validado = validarFormAtencion();
+      $('#botonGuardarAtencion').attr("disabled","disabled").delay(5000);
+      if(validado == true) {
+            // guardar la informacion personal del alumnos ingresada
+          $.post("<?php echo url_for('atenciones/create'); ?>",
+            $('#formGuardarAtencion').serialize(),
+          function(data){
+            var obj = jQuery.parseJSON(data);
+            $('#mensajeSuccess').html('Registro ingresado correctamente!!');
+            $('#mensajeError').html('');
+          }
+          );
+          
+    } else {
+      $('#mensajeError').html(validado);
+      $('#mensajeSuccess').html('');
+    } 
+      window.setTimeout( function(){ $('#botonGuardarAtencion').removeAttr("disabled") }, 5000 );
+      //$('#botonGuardarInfoPersonal').removeAttr("disabled");
+    return false;
+    }); 
+
+    $('#atenciones_idtratamiento').change(function(){
+        cargarImportes($("#atenciones_idtratamiento").val());
+    });   
+    
+     
+});
+
+function validarFormAtencion(){
+  var resultado = true;
+  if($("#atenciones_idtratamiento").val() <= 0) {
+    resultado = "Debe seleccionar un Tratamiento.";
+  } 
+
+  return resultado;
+} 
+
+//Cargar estudios previos
+function cargarImportes(id){
+  // obtener la lista de estudios previos de la persona
+  $.get("<?php echo url_for('tratamientos/obtenertratamiento'); ?>",
+      { idtratamiento: id },
+    function(data){
+      var fields = data.split('|');
+
+      var importe = fields[0];
+      var coseguro = fields[1];
+      $('#atenciones_importe').val(importe);
+      $('#atenciones_coseguro').val(coseguro);
+    }
+  );    
+} 
+
+</script>
+
 <?php use_stylesheets_for_form($form) ?>
 <?php use_javascripts_for_form($form) ?>
 
-<form action="<?php echo url_for('atenciones/'.($form->getObject()->isNew() ? 'create' : 'update').(!$form->getObject()->isNew() ? '?id='.$form->getObject()->getId() : '')) ?>" method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
+<a title="Volver a la ficha del Paciente" href="<?php echo url_for('atenciones/editar?id='.$idpaciente) ?>"><?php echo 'Volver a la ficha del Paciente' ?></a>
+
+<form action="" id="formGuardarAtencion" method="post">
 <input type="hidden" name="idpaciente" id="idpaciente" value="<?php echo $idpaciente ?>">
 <?php if (!$form->getObject()->isNew()): ?>
 <input type="hidden" name="sf_method" value="put" />
@@ -13,12 +79,22 @@
         <td colspan="6">
           &nbsp;
           <?php echo $form->renderHiddenFields(false) ?>
-          <input type="submit" value="Guardar" />
+          <input type="submit" value="Guardar" id="botonGuardarAtencion"/>
         </td>
       </tr>
     </tfoot>
     <tbody>
       <?php echo $form->renderGlobalErrors() ?>
+      <tr>
+          <td colspan="4">
+            <b><font color="red"><div align="center" id="mensajeError"></div></font></b>
+          </td>
+      </tr> 
+       <tr>
+          <td colspan="4">
+            <b><font color="green"><div align="center" id="mensajeSuccess"></div></font></b>
+          </td>
+      </tr> 
       <tr>
           <td colspan="2"><?php echo ''.'</b>' ?></td>
           <td colspan="2"><?php echo '<b>'.'Paciente : '.$paciente->getApellido().', '.$paciente->getNombre().'</b>' ?></td>
