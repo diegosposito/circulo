b8f4a789901756dc70b0da0ff2e4b0d6d43cdb3c
diegosposito <spositod@gmail.com>
2017-05-29 21:15:08 -0300

    cabmios

--- lib/model/doctrine/AtencionesTable.class.php
+++ lib/model/doctrine/AtencionesTable.class.php
@@ -62,7 +62,7 @@ class AtencionesTable extends Doctrine_Table
         $sql ="SELECT at.id, at.nrodoc, at.mes, at.anio, at.matricula, at.fecha, at.pieza, at.caras, at.tratamiento, at.importe, at.coseguro, at.bono, at.importe,
             pac.apellido, pac.nombre, os.denominacion, os.abreviada as obrasocial, concat(per.apellido,', ' , per.nombre) as profesional,
             CASE at.mes WHEN 1 THEN 'Enero' WHEN 2 THEN 'Febrero' WHEN 3 THEN 'Marzo' WHEN 4 THEN 'Abril' WHEN 5 THEN 'Mayo' WHEN 6 THEN 'Junio' WHEN 7 THEN 'Julio' WHEN 8 THEN 'Agosto' WHEN 9 THEN 'Septiembre' WHEN 10 THEN 'Octubre' WHEN 11 THEN 'Noviembre' WHEN 12 THEN 'Diciembre'   ELSE 'Enero'  END as mesdetalle,
-            CASE at.idestadoatencion WHEN 1 THEN 'Abierta' WHEN 2 THEN 'Cerrada' END as estadoatencion
+            CASE at.idestadoatencion WHEN 1 THEN 'Abierta' WHEN 0 THEN 'Cerrada' END as estadoatencion
             FROM atenciones at JOIN pacientes pac ON at.nrodoc = pac.nrodoc
             JOIN obras_sociales os ON at.idobrasocial = os.idobrasocial
             JOIN personas per ON at.matricula = per.nrolector
@@ -83,4 +83,17 @@ class AtencionesTable extends Doctrine_Table
 
         return $q;
     }
+
+    // Cerrar atenciones por periodo por Profesional
+    public static function cerrarAtencionesPorProfesionalPeriodo($matricula, $idmes, $idanio)
+    {
+
+        $sql ="UPDATE atenciones SET idestadoatencion = 0 WHERE matricula = '".$matricula."' AND anio = ".$idanio." AND mes = ".$idmes.";";
+
+        $q = Doctrine_Manager::getInstance()->getCurrentConnection();
+
+        return $q->execute($sql);
+
+    }
+
 }
