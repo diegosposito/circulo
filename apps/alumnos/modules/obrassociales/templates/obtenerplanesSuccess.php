640037a9913ac9813847245d28facf51706e546a
Sposito, Diego <spositod@gmail.com>
2017-06-01 11:55:54 -0300

    cabmios

--- apps/alumnos/modules/obrassociales/templates/obtenerplanesSuccess.php
+++ apps/alumnos/modules/obrassociales/templates/obtenerplanesSuccess.php
@@ -2,7 +2,12 @@
 if (count($planes) > 0){
 	//el bucle para cargar las opciones
 	foreach ($planes as $plan){
-		echo "<option value=".$plan->getId().">".$plan->getNombre()."</option>";
+		if ($plan->getId()==$idplan){
+			 echo "<option value=".$plan->getId().">".$plan->getNombre()."</option>";
+		} else {
+			 echo "<option value=".$plan->getId()." selected>".$plan->getNombre()."</option>";
+		}
+
 	}
 }
 ?>
