e4ddda5d2c6e5df5e826f7516db6e6967921ca73
diegosposito <spositod@gmail.com>
2017-05-31 21:23:57 -0300

    cambios

--- apps/alumnos/modules/planesobras/templates/indexSuccess.php
+++ apps/alumnos/modules/planesobras/templates/indexSuccess.php
@@ -15,7 +15,8 @@
         <td width="5%" align="center" class="hed">idOSocial</td>
         <td width="60%" align="center" class="hed">Obra Social</td>
         <td width="5%" align="center" class="hed">idPlan</td>
-        <td width="30%" align="center" class="hed">Plan</td>
+        <td width="25%" align="center" class="hed">Plan</td>
+        <td width="5%" align="center" class="hed">Activo</td>
         <td width="10%" align="center" class="hed">Edicion</td>
       </tr>
     </thead>
@@ -26,7 +27,15 @@
         <td width="5%"><?php echo  $planes_obras->getObrasSociales()->getIdObrasocial() ?></td>
         <td width="70%"><?php echo  $planes_obras->getObrasSociales()->getAbreviada() ?></td>
         <td width="5%"><?php echo  $planes_obras->getId() ?></td>
-        <td width="30%" align="center"><?php echo $planes_obras->getNombre() ?></td>
+        <td width="25%" align="center"><?php echo $planes_obras->getNombre() ?></td>
+        <td align='center' width="5%">
+        <?php 
+          if ($planes_obras->getActivo()){ ?>
+            <img width="17px" height="17px" src='<?php echo $sf_request->getRelativeUrlRoot();?>/images/accept_ico.png' align='center' size='20' />
+          <?php } else { ?>
+             <img width="14px" height="14px"  src='<?php echo $sf_request->getRelativeUrlRoot();?>/images/wrong_ico.png' align='center' size='20' />
+          <?php } ?> 
+        </td>
         <td align="center"><?php echo link_to("Editar", 'planesobras/edit?id='.$planes_obras->getId() ,'class="mhead"'); ?></td>
       </tr>
       <?php $i++; ?>
