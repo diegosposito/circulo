c002a6dd6fbbfe6599ad627ab8e1bc79cfabffc6
Sposito, Diego <spositod@gmail.com>
2017-06-09 11:55:49 -0300

    cabmios

--- lib/form/doctrine/BannersForm.class.php
+++ lib/form/doctrine/BannersForm.class.php
@@ -13,33 +13,36 @@ class BannersForm extends BaseBannersForm
   public function configure()
   {
   	   unset( $this['created_at'], $this['updated_at'], $this['created_by'], $this['updated_by'] );
-     
+
       // Se define los labels
 	  $this->widgetSchema->setLabel('nombre', '<p align="left">Nombre de Archivo:</p>');
  	  $this->widgetSchema->setLabel('visible', '<p align="left">Visible:</p>');
  	  $this->widgetSchema->setLabel('url', '<p align="left">URL:</p>');
+    $this->widgetSchema->setLabel('coordenada', '<p align="left">Coordenada:</p>');
+    $this->widgetSchema->setLabel('urlsecundaria', '<p align="left">URL Secundaria:</p>');
+    $this->widgetSchema->setLabel('coordenadasec', '<p align="left">Coord Secundaria:</p>');
       $this->widgetSchema->setLabel('imagefile', '<p align="left">Imágen:</p>');
        $this->widgetSchema->setLabel('idorden', '<p align="left">Orden:</p>');
 
-     
+
      // $arregloCategorias = array('1' => 'General', '2' => 'Para Profesional');
 
       $this->widgetSchema['imagefile'] = new sfWidgetFormInputFile(array(
                                                'label' => 'Archivo',
                                              ));
 
-     
+
       $this->widgetSchema['nombre'] = new sfWidgetFormInputText(array(), array("style"=>'width: 200px;'));
       $this->widgetSchema['url'] = new sfWidgetFormInputText(array(), array("style"=>'width: 400px;'));
-    
-     
-     
+
+
+
       $this->widgetSchema->setLabel('imagefile', '<p align="left">Archivo:</p>');
        $this->widgetSchema->setLabel('nombre', '<p align="left">Nombre de Archivo:</p>');
         $this->widgetSchema->setLabel('idorden', '<p align="left">Orden:</p>');
         $this->widgetSchema->setLabel('url', '<p align="left">URL:</p>');
-    
-      
+
+
       $this->setValidators(array(
         'nombre' => new sfValidatorString(array('required' => true), array('required' => 'El nombre es obligatorio.')),
         'idorden'    => new sfValidatorInteger(array('required' => false)),
@@ -47,6 +50,6 @@ class BannersForm extends BaseBannersForm
         'url' => new sfValidatorString(array('required' => false, )),
         ));
 
-      $this->validatorSchema->setOption('allow_extra_fields',true); 
+      $this->validatorSchema->setOption('allow_extra_fields',true);
   }
 }
