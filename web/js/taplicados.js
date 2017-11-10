72acc12f8d29407c616ae67d6edbf789ee7798cd
diegosposito <spositod@gmail.com>
2017-11-09 22:16:18 -0300

    archivo de prueba

---
+++ web/js/taplicados.js
@@ -0,0 +1,4 @@
+[
+	{"diente":{"id":17,"x":25,"y":0},"cara":"C","tratamiento":{"id":"10.16.01","nombre":"Hasta 1 cm. de diámetro","aplicaCara":true,"aplicaDiente":true,"color":"blue"}},
+	{"diente":{"id":17,"x":25,"y":0},"cara":"S","tratamiento":{"id":"10.16.01","nombre":"Hasta 1 cm. de diámetro","aplicaCara":true,"aplicaDiente":true,"color":"blue"}}
+]
