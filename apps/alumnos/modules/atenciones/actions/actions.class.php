ba8c9cb0b8af9f251bc902044101a139d278b090
Sposito, Diego <spositod@gmail.com>
2017-11-14 09:42:37 -0300

    actions

--- apps/alumnos/modules/atenciones/actions/actions.class.php
+++ apps/alumnos/modules/atenciones/actions/actions.class.php
@@ -265,7 +265,7 @@ class atencionesActions extends sfActions
 
      // CUARTO TAB
      // Obtener json desde base de datos con atenciones para Odontograma
-     $this->jsonatenciones = '[{"diente":{"id":17,"x":25,"y":0},"cara":"S","tratamiento":{"id":"10.16.01","nombre":"Hasta 1 cm. de diámetro","aplicaCara":true,"aplicaDiente":true,"color":"blue"}},{"diente":{"id":18,"x":50,"y":0},"cara":"C","tratamiento":{"id":"10.16.01","nombre":"Hasta 1 cm. de diámetro","aplicaCara":true,"aplicaDiente":true,"color":"blue"}}]';
+     $this->jsonatenciones = '[{"diente":{"id":17,"x":25,"y":0},"cara":"S","tratamiento":{"id":"10.16.01","nombre":"Hasta 1 cm. de diámetro","aplicaCara":true,"aplicaDiente":true,"color":"blue"}},{"diente":{"id":18,"x":50,"y":0},"cara":"C","tratamiento":{"id":"10.16.01","nombre":"Hasta 1 cm. de diámetro","aplicaCara":true,"aplicaDiente":true,"color":"red"}}]';
   }
 
   public function executeMostrarficha(sfWebRequest $request)
