5a71fa223eac6a25e221bad104f290e33ba342d3
diegosposito <spositod@gmail.com>
2017-12-05 21:47:22 -0300

    cambiar puente

--- web/js/tratamientos.js
+++ web/js/tratamientos.js
@@ -1,11 +1,11 @@
 [
 	{ "id":"01.01", "nombre":"Restauración Caries", "aplicaCara":true, "aplicaDiente":false ,"color":"red"},
 	{ "id":"01.02", "nombre":"Extracción-Pieza ausente", "aplicaCara":false, "aplicaDiente":true ,"color":"blue"},
-	{ "id":"01.03", "nombre":"Prótesis Fija", "aplicaCara":false, "aplicaDiente":true ,"color":"blue"},
+	{ "id":"01.03", "nombre":"Puente", "aplicaCara":false, "aplicaDiente":true ,"color":"blue"},
 	{ "id":"01.04", "nombre":"Implante", "aplicaCara":false, "aplicaDiente":true ,"color":"blue"},
 	{ "id":"01.05", "nombre":"Corona", "aplicaCara":false, "aplicaDiente":true ,"color":"blue"},
 	{ "id":"01.06", "nombre":"Tratamiento de conducto", "aplicaCara":false, "aplicaDiente":true ,"color":"blue"},
 	{ "id":"01.07", "nombre":"Radiografía", "aplicaCara":false, "aplicaDiente":true ,"color":"blue"},
 	{ "id":"01.08", "nombre":"Perno Muñon", "aplicaCara":false, "aplicaDiente":true ,"color":"blue"},
-	{ "id":"01.09", "nombre":"Puente", "aplicaCara":false, "aplicaDiente":true ,"color":"blue"}
+	{ "id":"01.09", "nombre":"Prótesis Fija", "aplicaCara":false, "aplicaDiente":true ,"color":"blue"}
 ]
